c7587a9de5c4d68ebff98c7145c24ba531a72e19
SafyaAbouelfadel <safia.abf@gmail.com>
2025-02-17 11:51:03 -0800

    modify basemodel for user password and add CORS and fix typos

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,15 +6,18 @@ from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 @app.teardown_appcontext
 def close_db(exception):
     """Close the database connection after each request."""
-    return strorage.close()
+    return storage.close()
 
 @app.errorhandler(404)
 def not_found(error):
